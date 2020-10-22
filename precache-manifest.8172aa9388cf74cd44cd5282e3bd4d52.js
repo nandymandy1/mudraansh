75ee61e45a7a339ab09a1c24a32a0ee56ce3f478
Nandy <nandymandy09@hotmail.com>
2020-10-22 15:42:22 +0530

    somthing

---
+++ precache-manifest.8172aa9388cf74cd44cd5282e3bd4d52.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3831894ab5bcf7907f17548267813692",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4704e486bcd683db4a4e",
+    "url": "/static/css/2.8cbc8ccb.chunk.css"
+  },
+  {
+    "revision": "8d5a30511ba446d91129",
+    "url": "/static/css/main.84e8b7c2.chunk.css"
+  },
+  {
+    "revision": "4704e486bcd683db4a4e",
+    "url": "/static/js/2.0342eb1d.chunk.js"
+  },
+  {
+    "revision": "0fc61ce0ee2d491f41ee7634c63d9b90",
+    "url": "/static/js/2.0342eb1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d5a30511ba446d91129",
+    "url": "/static/js/main.0342fd9a.chunk.js"
+  },
+  {
+    "revision": "60c1290b5e35e4c607cd",
+    "url": "/static/js/runtime-main.353b164e.js"
+  },
+  {
+    "revision": "92b006cb86f4f0e0e26701873209fc11",
+    "url": "/static/media/bg-1.92b006cb.png"
+  },
+  {
+    "revision": "19cd5857477b5118400b3eaf1b6b14ea",
+    "url": "/static/media/bg-2.19cd5857.jpg"
+  },
+  {
+    "revision": "087bca36ab52af43fa5079a839bc266f",
+    "url": "/static/media/bg-3.087bca36.jpg"
+  },
+  {
+    "revision": "a4a686924a20dc0a1247d226d0520aed",
+    "url": "/static/media/bullet-point.a4a68692.png"
+  },
+  {
+    "revision": "55404265043be037bf990ec1cc292eac",
+    "url": "/static/media/clock.55404265.png"
+  },
+  {
+    "revision": "e0417fbbaae8751203e08e0d7e4d862e",
+    "url": "/static/media/housing.e0417fbb.jpg"
+  },
+  {
+    "revision": "0f20dc6dc9a89dcffbd19e20afba0c82",
+    "url": "/static/media/logo.0f20dc6d.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
